1f6b15ba913096832503a63feed450e6720b421a
pj1301 <hayward.jp@gmail.com>
2020-06-24 19:08:16 +0100

    working program

--- src/db.js
+++ src/db.js
@@ -3,6 +3,7 @@ const test = require('./dbRouteData/test.json');
 function getRoutes() {
   return {
     "/api/v1/test": "/test",
+    "/api/v1/test/:id": "/test/:id"
     // Add new paths here - note that there must be a '/' at the beginning of the key and value, the value should go no further than a second level
   };
 }
